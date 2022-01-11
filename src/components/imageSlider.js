2964f47b951fba6992f40f8f2d53cb3ff2f835e4
chandrakiran343 <chandra.dkiran@outlook.com>
2022-01-11 11:54:46 +0530

    Huge changes to main page/ image slider

--- src/components/imageSlider.js
+++ src/components/imageSlider.js
@@ -5,10 +5,9 @@ const imageSlider = () => {
     
     function open(src){
         window.open(src,'_blank').focus();
-
     }
 
-    const imageArray = ['https://kbimages1-a.akamaihd.net/a905ae89-1a86-4d04-b6fc-b91d08405d78/1200/1200/False/martial-peak-vol-1.jpg','https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1493321941l/34999563.jpg','https://images-na.ssl-images-amazon.com/images/I/71aFt4+OTOL.jpg','https://1.bp.blogspot.com/-cej3QQg3YnM/X7kQb_4zZqI/AAAAAAABEj0/j1S2ujzz0AkBBv7kNezGCVjUnZPSP6QqgCLcBGAsYHQ/s1600/2%2B-%2BTales%2Bof%2Bdemons%2Band%2Bgods%2BChapter%2B302.jpeg','https://upload.wikimedia.org/wikipedia/en/7/7d/Tower_of_God_Volume_1_Cover.jpg','https://images-na.ssl-images-amazon.com/images/I/81ZAC67DE1S.jpg']
+    const imageArray = ['http://img.timeinc.net/time/magazine/archive/covers/2000/1101000619_400.jpg','https://images-na.ssl-images-amazon.com/images/I/51FooYXn34L.jpg','https://cdna.artstation.com/p/assets/images/images/009/360/872/large/luca-lonardi-cover-it-nologo.jpg?1518543299&dl=1','https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1552940591l/2574278._SY475_.jpg','https://upload.wikimedia.org/wikipedia/en/7/7d/Tower_of_God_Volume_1_Cover.jpg','https://images-na.ssl-images-amazon.com/images/I/91vj7Wi8WsL.jpg']
     
     
     return (<div className="slider-container">
--- src/components/mainPage.js
+++ src/components/mainPage.js
@@ -1,24 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './mainPage.css'
 import ImageSlider from './imageSlider'
+import Card from './card.js'
 
-const mainPage = () => {
 
+const MainPage = () => {
+
+    const [search,setSearch] = useState('')
+
+    const handleSearch = () => {
+        console.log(search)
+    }
+
+    
     return (<div className="container-style">
-    <hr className="hr-style"></hr>
-    <span className="heading-style"><span>BookStore</span></span>
-    <hr className="hr-style"></hr>
+    <hr className="hr-style"></hr>  
+    <div className="header">
+    <span className="heading-style">BookStore
+    <input onChange={(e)=>{setSearch(e.target.value)}} style={{fontSize:"20px",fontFamily:'sans-serif',marginLeft:'200px',marginRight:'10px',borderColor:'purple'}} type="text"/>
+    <button type='submit' onClick={()=>{handleSearch()}} className="btn btn-success"><i class="fas fa-search"></i></button>
+    </span>
     <nav id='navi' className="nav-style">
     <button style={{margin:'10px 20px 10px 20px'}} className="btn btn-primary"><span className="navbtn-style">Home</span></button>
     <button style={{margin:'10px 20px 10px 20px'}} className="btn btn-info" ><span className="navbtn-style">About Us</span></button>
-    <button style={{margin:'10px 20px 10px 20px'}} className="btn btn-fas fa-"><span className="navbtn-style">Contact Us</span></button>
-    <button style={{margin:'10px 20px 10px 20px'}} className="btn btn-fas fa-"><span className="navbtn-style">Menu</span></button>
+    <button style={{margin:'10px 20px 10px 20px'}} className="btn btn-info"><span className="navbtn-style">Contact Us</span></button>
+    <button style={{margin:'10px 20px 10px 20px'}} className="btn btn-info"><span className="navbtn-style">Menu</span></button>
     <button style={{margin:'10px 20px 10px 20px'}} className="btn btn-success"><span className="navbtn-style">Order History</span></button>
     </nav>
+    </div>
     <hr className="hr-style"></hr>
+    <div className="body">
     <ImageSlider />
-    
+    <div className='card-container'>
+        <Card text='hello' imgurl="https://tibs2.threeifbyspace.net/wp-content/uploads/2020/05/visual.jpg" />
+        <Card text='hello' imgurl="https://tibs2.threeifbyspace.net/wp-content/uploads/2020/05/visual.jpg" />
+        <Card text='hello' imgurl="https://tibs2.threeifbyspace.net/wp-content/uploads/2020/05/visual.jpg" />
+
+    </div></div>
         </div>);
 }
  
-export default mainPage;
+export default MainPage;
