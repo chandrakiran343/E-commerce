3a802e57b6e5fe9a814f6eb79b99982d39a44f26
karthik-924 <20p61a1240@vbithyd.ac.in>
2022-01-11 10:16:57 +0530

    commit message

--- src/components/mainPage.js
+++ src/components/mainPage.js
@@ -6,14 +6,14 @@ const mainPage = () => {
 
     return (<div className="container-style">
     <hr className="hr-style"></hr>
-    <span className="heading-style"><span>BookStore</span></span>
+    <span className="heading-style"><span>Book Store</span></span>
     <hr className="hr-style"></hr>
     <nav id='navi' className="nav-style">
-    <button style={{margin:'10px 20px 10px 20px'}} className="btn btn-primary"><span className="navbtn-style">Home</span></button>
-    <button style={{margin:'10px 20px 10px 20px'}} className="btn btn-info" ><span className="navbtn-style">About Us</span></button>
+    <button style={{margin:'10px 20px 10px 20px'}} className="btn btn-fas fa-"><span className="navbtn-style">Home</span></button>
+    <button style={{margin:'10px 20px 10px 20px'}} className="btn btn-fas fa-" ><span className="navbtn-style">About Us</span></button>
     <button style={{margin:'10px 20px 10px 20px'}} className="btn btn-fas fa-"><span className="navbtn-style">Contact Us</span></button>
     <button style={{margin:'10px 20px 10px 20px'}} className="btn btn-fas fa-"><span className="navbtn-style">Menu</span></button>
-    <button style={{margin:'10px 20px 10px 20px'}} className="btn btn-success"><span className="navbtn-style">Order History</span></button>
+    <button style={{margin:'10px 20px 10px 20px'}} className="btn btn-fas fa-"><span className="navbtn-style">Order History</span></button>
     </nav>
     <hr className="hr-style"></hr>
     <ImageSlider />
