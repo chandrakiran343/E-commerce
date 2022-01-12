59bc6a089e1d908cbcd009c1c543179611e2a9f8
karthik-924 <20p61a1240@vbithyd.ac.in>
2022-01-12 10:04:03 +0530

    changes

--- src/components/mainPage.js
+++ src/components/mainPage.js
@@ -16,16 +16,18 @@ const MainPage = () => {
     return (<div className="container-style">
     <hr className="hr-style"></hr>  
     <div className="header">
-    <span className="heading-style">BookStore
-    <input onChange={(e)=>{setSearch(e.target.value)}} style={{fontSize:"20px",fontFamily:'sans-serif',marginLeft:'200px',marginRight:'10px',borderColor:'purple'}} type="text"/>
+    <span className="heading-style">BookStore</span>
+    <span  id='space'>
+    
+    <input  onChange={(e)=>{setSearch(e.target.value)}} style={{fontSize:"20px",fontFamily:'sans-serif',marginLeft:'200px',marginRight:'10px',borderColor:'purple'}} type="text"/>
     <button type='submit' onClick={()=>{handleSearch()}} className="btn btn-success"><i class="fas fa-search"></i></button>
     </span>
     <nav id='navi' className="nav-style">
-    <button style={{margin:'10px 20px 10px 20px'}} className="btn btn-primary"><span className="navbtn-style">Home</span></button>
-    <button style={{margin:'10px 20px 10px 20px'}} className="btn btn-info" ><span className="navbtn-style">About Us</span></button>
-    <button style={{margin:'10px 20px 10px 20px'}} className="btn btn-info"><span className="navbtn-style">Contact Us</span></button>
-    <button style={{margin:'10px 20px 10px 20px'}} className="btn btn-info"><span className="navbtn-style">Menu</span></button>
-    <button style={{margin:'10px 20px 10px 20px'}} className="btn btn-success"><span className="navbtn-style">Order History</span></button>
+    <button style={{margin:'10px 20px 10px 20px'}} className="btn btn-default"><span className="navbtn-style">Home</span></button>
+    <button style={{margin:'10px 20px 10px 20px'}} className="btn btn-default"><span className="navbtn-style">About Us</span></button>
+    <button style={{margin:'10px 20px 10px 20px'}} className="btn btn-default"><span className="navbtn-style">Contact Us</span></button>
+    <button style={{margin:'10px 20px 10px 20px'}} className="btn btn-default"><span className="navbtn-style">Menu</span></button>
+    <button style={{margin:'10px 20px 10px 20px'}} className="btn btn-default"><span className="navbtn-style">Order History</span></button>
     </nav>
     </div>
     <hr className="hr-style"></hr>
